64564c99c6d89e64c6f8f25eed3a002ca5bc5825
will.shull@gmail.com <will.shull@gmail.com>
2015-06-11 02:44:06 -0700

    added jquery ready event handler. changed the losing image opacity from .5 to .25

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,72 +1,82 @@
-function Kitten(photo, votes) {
-  this.photo = photo;
-  this.votes = 0;
-}
+$(function() {
 
-var kittenList = []
-for(i=0; i<14; i++) {
-  kittenList.push(new Kitten("images/kitten" + i, 0));
-}
+  function Kitten(photo, votes) {
+    this.photo = photo;
+    this.votes = 0;
+  }
 
-var randomGenerator = function() {
-  var rand = Math.floor(Math.random() * kittenList.length);
-  return kittenList[rand];
-}
+  var kittenList = []
+  for(i=0; i<14; i++) {
+    kittenList.push(new Kitten("images/kitten" + i, 0));
+  }
 
-function Init() {
-  var imgLeft, imgRight, randomA, randomB, winner, targetParent;
-  $('.votesDisplay').hide();
+  var randomGenerator = function() {
+    var rand = Math.floor(Math.random() * kittenList.length);
+    return kittenList[rand];
+  }
 
-  randomA = randomGenerator();
-  do {
-    randomB = randomGenerator();
-  } while(randomA == randomB);
-
-  imgLeft = randomA;
-  imgRight = randomB;
-  $('#photoLeft').attr('src', imgLeft.photo);
-  $('#photoRight').attr('src', imgRight.photo);
-
-  //Vote listener
-  $('.catImage').on('click', function() {
-    //target element
-    var userTarget = $(this);
-    targetParent = userTarget.parent().attr('id');
-    //target img source
-    var targetSrc = userTarget.attr('src');
-    if (targetSrc == imgLeft.photo) {
-      imgLeft.votes++
-      winner = imgLeft;
-    } else {
-      imgRight.votes++
-      winner = imgRight;
-    }
-    //voteCountLeft/Right
-    $('#voteCountLeft').text(imgLeft.votes);
-    $('#voteCountRight').text(imgRight.votes);
-    $('.votesDisplay').show();
-  });
-
-
-  //Next image button
-  $('.nextButton').on('click', function() {
-    var newRand;
+  function Init() {
+    var imgLeft, imgRight, randomA, randomB, winner, targetParent;
     $('.votesDisplay').hide();
+    $('.nextButton').hide();
 
+    randomA = randomGenerator();
     do {
-      newRand = randomGenerator();
-    } while(newRand == winner);
+      randomB = randomGenerator();
+    } while(randomA == randomB);
 
-    if (targetParent == "catPhotoLeft") {
-      imgRight = newRand;
-      $('#photoRight').attr('src', imgRight.photo);
-    } else {
-      imgLeft = newRand;
-      $('#photoLeft').attr('src', imgLeft.photo);
-    }
+    imgLeft = randomA;
+    imgRight = randomB;
+    $('#photoLeft').attr('src', imgLeft.photo);
+    $('#photoRight').attr('src', imgRight.photo);
 
-  })
+    //Vote listener
+    $('.catImage').on('click', function() {
+      //target element
+      var userTarget = $(this);
+      targetParent = userTarget.parent().attr('id');
+      //target img source
+      var targetSrc = userTarget.attr('src');
+      if (targetSrc == imgLeft.photo) {
+        imgLeft.votes++
+        winner = imgLeft;
+        $('#photoRight').addClass('loserOpacity');
+      } else {
+        imgRight.votes++
+        winner = imgRight;
+        $('#photoLeft').addClass('loserOpacity');
+      }
+      //voteCountLeft/Right
+      $('#voteCountLeft').text(imgLeft.votes);
+      $('#voteCountRight').text(imgRight.votes);
+      $('.votesDisplay').show();
+      $('.nextButton').show();
+    });
 
-}
 
-var start = new Init();
+    //Next image button
+    $('.nextButton').on('click', function() {
+      $(this).hide();
+      var newRand;
+      $('.votesDisplay').hide();
+
+      do {
+        newRand = randomGenerator();
+      } while(newRand == winner);
+
+      if (targetParent == "catPhotoLeft") {
+        imgRight = newRand;
+        $('#photoRight').removeClass('loserOpacity');
+        $('#photoRight').attr('src', imgRight.photo);
+      } else {
+        imgLeft = newRand;
+        $('#photoLeft').removeClass('loserOpacity');
+        $('#photoLeft').attr('src', imgLeft.photo);
+      }
+
+    })
+
+  }
+
+  var start = new Init();
+});
